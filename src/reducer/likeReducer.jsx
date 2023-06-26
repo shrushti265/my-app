93b9ec5b5b2539ac9039036a8acc513cd62e335c
shrushti265 <shruy26@gmail.com>
2023-06-26 23:17:30 +0530

    create likeContext

--- src/reducer/likeReducer.jsx
+++ src/reducer/likeReducer.jsx
@@ -1,12 +1,13 @@
 const likeReducer = (likeState, likeAction) => {
     switch (likeAction.type) {
         case "ADD_TO_LIKE":
- return {...likeState, 
-  likedItems : [...likeState.likedItems, likeAction.payload]
+ return {
+    ...likeState, 
+    likedItems : [...likeState.likedItems, likeAction.payload]
 }            
 
         default:
             return likeState
     }
 }
-export{likeReducer}
+export { likeReducer }
